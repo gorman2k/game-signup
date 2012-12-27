f13d986cfd83d5ec1c633a8421d315204075d8dd
Barrett Cervenka <barrett@cervenka.com>
2012-12-27 10:58:33 -0600

    add admin user/pass env vars

--- seedUsers.rb
+++ seedUsers.rb
@@ -1,8 +1,10 @@
 require 'mongo'
 include Mongo
 
-db = Connection.new.db('poker')
-users = db.collection('users')
+mongo_uri = ENV['MONGOHQ_URL']
+@client = MongoClient.from_uri(mongo_uri)
+@db = @client.db(ENV['MONGOHQ_DB'])
+@users = @db.collection('users')
 
 begin
 	file = File.new("emails.txt", "r")
@@ -10,7 +12,7 @@ begin
 		# file format is: email#firstName#lastName
 		user = "#{line}".chomp.split('#')
 		new_user = { :email => user[0].downcase, :firstName => user[1], :lastName => user[2], :created_on => Time.now }
-		users.insert(new_user)
+		@users.insert(new_user)
 	end
 	file.close
 rescue => err
